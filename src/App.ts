1b7cfcea812513edfa62eaef16a81323551bafdc
Arthur Mergulhão <alves.m.arthur@gmail.com>
2022-09-08 13:48:55 -0300

    Agora servidor consegue servir arquivos estaticos

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,8 @@ import express from "express";
 import * as bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import * as path from 'path';
+
 
 import routes from "./routes/routes";
 import swagger from './routes/swagger';
@@ -20,6 +22,7 @@ class App {
   }
 
   private async setup() {
+    this.server.use('/static', express.static(path.join(__dirname, '../public/')));
     this.server.use(express.json());
     this.server.use(cors());
     this.server.use(bodyParser.json());
