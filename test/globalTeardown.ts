8e1fd28ee5abccdb18d4864ac787d8749cab4961
Arthur Mergulhão <alves.m.arthur@gmail.com>
2022-09-08 14:53:30 -0300

    Fechando conexão em caso de erro no teardown

--- test/globalTeardown.ts
+++ test/globalTeardown.ts
@@ -1,20 +1,26 @@
 import dotenv from 'dotenv';
 
-import { clearDatabase, createDatabaseConnection } from "../src/utils/database";
+import { clearDatabase, closeDatabaseConnection, createDatabaseConnection } from "../src/utils/database";
 
 const globalTeardown = async (): Promise<void> => {
-    console.log('Starting test environment teardown.')
-    dotenv.config({
-        path: `.env.${process.env.NODE_ENV}`,
-    });
+    try {
+        console.log('Starting test environment teardown.')
+        dotenv.config({
+            path: `.env.${process.env.NODE_ENV}`,
+        });
 
-    await createDatabaseConnection().then(() => {
-        console.log('Database connection stablished.');
-    })
-    console.log('Starting test database cleanup');
-    await clearDatabase().then(() => {
-        console.log('Database cleanup completed')
-    });
+        await createDatabaseConnection().then(() => {
+            console.log('Database connection stablished.');
+        })
+        console.log('Starting test database cleanup');
+        await clearDatabase().then(() => {
+            console.log('Database cleanup completed')
+        })
+    } catch (error) {
+        await closeDatabaseConnection()
+        console.error(error.message);
+    }
+
 }
 
 export default globalTeardown();
