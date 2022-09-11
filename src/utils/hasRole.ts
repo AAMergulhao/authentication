bbaf4721bf41dbb9bcd27d471bbf15e6ad92c9f1
Arthur Mergulhão <alves.m.arthur@gmail.com>
2022-09-11 00:15:39 -0300

    Removendo arquivo que não é mais utilizado

--- src/utils/hasRole.ts
+++
@@ -1,9 +0,0 @@
-const hasRole = (userRoles: string[], allowedRoles: string[]): boolean => {
-  for (const role of allowedRoles) {
-    if (userRoles.includes(role)) return true;
-  }
-
-  return false;
-};
-
-export default hasRole;
--- test/utils/hasRole.test.ts
+++
@@ -1,9 +0,0 @@
-import hasRole from "../../src/utils/hasRole";
-
-test("Should return true", async () => {
-  expect(hasRole(['ADMIN'], ['ADMIN', 'STANDARD'])).toBe(true);
-});
-
-test("Should return false", async () => {
-  expect(hasRole(['TEST'], ['ADMIN', 'STANDARD'])).toBe(false);
-});
